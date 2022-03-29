5d6f31af50d659b4609945fa366c508d1d5871b7
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-03-29 11:16:59 -0500

    add structure redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import state from './redux/store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={state}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/redux/reducers/reducerUser.ts
@@ -0,0 +1,24 @@
+/**
+ * import .... from 'interface'
+ * 
+ * 
+ */
+
+import {Actions, User, TYPES} from '../interface';
+
+type USER=null | User;
+
+const INITIAL_USER:USER=null;
+
+
+export const userReducer = (state:USER=INITIAL_USER, action: Actions): USER => {
+
+  switch (action.type) {
+
+    case TYPES.CREATE_USER:
+        return action.payload
+
+    default:
+      return state;
+  }
+};
