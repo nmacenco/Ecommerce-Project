e83053ad787a224ae6c1fc60faf83a8c5e293e7b
Julian Gonzalez <julianmarcos505@gmail.com>
2022-03-30 13:21:01 -0300

    product component

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return(
-        <div>
-            <h1>BACANO</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      
+
+    </>
+  );
+};
 
 export default Home;
