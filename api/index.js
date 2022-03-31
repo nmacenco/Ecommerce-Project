2ef0002875942132251a39e082516566878d8dc5
jaider012 <jaiderandres901@hotmail.com>
2022-03-30 21:16:14 -0500

    brand get and post

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 const PORT = process.env.PORT || 3001;
+
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`); // eslint-disable-line no-console
--- api/src/controllers/brand.js
+++ api/src/controllers/brand.js
@@ -1,7 +1,30 @@
-const { Brand } = require("../db");
+const { Brand, Product_Brand } = require("../db");
 
-const getBrands = (req, res, next) => {};
+const getBrands = async (req, res, next) => {
+  try {
+    let databrand = await Brand.findAll({
+      //find the game in the database and retuFrn
+    });
+    res.status(200).json(databrand);
+  } catch (error) {
+    res.status(500);
+    console.log(error);
+  }
+};
 
-const createBrand = (req, res, next) => {};
+const createBrand = async (req, res, next) => {
+  try {
+    let { name } = req.body;
+    if (!name) {
+      res.status(404).send("Falta data");
+    } else {
+      const newbrand = await Brand.create({ name });
+      console.log(newbrand);
+      res.status(200).json(newbrand);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = { getBrands, createBrand };
