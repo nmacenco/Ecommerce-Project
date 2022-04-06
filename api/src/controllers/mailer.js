8556adf6e020465d58a10f74b00234daed6ddffa
Carlos Mario Espinosa Restrepo <carlosespinosa66@outlook.com>
2022-04-06 10:52:08 -0500

    NEW (Emails Sent)

---
+++ api/src/controllers/mailer.js
@@ -0,0 +1,24 @@
+const mailer = require("../utils/mailer-setup");
+
+const sendMail = async (req, res) => {
+    let { emailToAddress, emailToSubject, emailToBody } = req.body;
+
+    try {
+
+        let info = await mailer.sendMail({
+            from: '"Ecommerce Henry PF" <najupasa@gmail.com>',
+            to: emailToAddress,
+            subject: emailToSubject,
+            html: emailToBody
+        });
+
+        info ?
+            res.status(200).send({
+                successMsg: "Mail successfully sent."
+            })
+            : res.status(401).json({ errorMsg: "Mail can not be sent." });
+    } catch (error) {
+        res.status(500).send({ errorMsg: error.message });
+    }
+};
+
