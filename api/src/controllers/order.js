5f7ca67308a4779df44e2c809551c48b77936e83
jaider012 <jaiderandres901@hotmail.com>
2022-04-05 19:43:25 -0500

    pull

--- api/src/controllers/order.js
+++ api/src/controllers/order.js
@@ -1,46 +1,39 @@
 const { Order, User } = require("../db");
 
-const getOrders = async (req, res, next) => {
-  try {
-    let Orders = await Order.findAll({
-      // include: [
-      //   {
-      //     model: User,
-      //     attributes: ["id", "name","surname","email" ],
-      //   },
-      // ],
-    });
-    if (!Orders.length) {
-      res.status(404).send({ errorMsg: "Oders not found." });
-    }
-    Orders = Orders.map((Order) => {
-      return {
-        name: Order.name,
-        id: Order.id,
-        total_amount: Order.total_amount,
-        email_address: Order.email_address,
-        status: Order.status,
-        billing_address: Order.billing_address,
-      };
-    });
-    res.status(200).send({ successMsg: "Here are your Orders.", Orders });
-  } catch (error) {
-    res.status(500).send({ errorMsg: error });
-  }
-};
+// const getOrders = async (req, res, next) => {
+//   try {
+//     let Orders = await Order.findAll({
+//       // include: [
+//       //   {
+//       //     model: User,
+//       //     attributes: ["id", "name","surname","email" ],
+//       //   },
+//       // ],
+//     });
+//     if (!Orders.length) {
+//       res.status(404).send({ errorMsg: "Oders not found." });
+//     }
+//     Orders = Orders.map((Order) => {
+//       return {
+//         name: Order.name,
+//         id: Order.id,
+//         total_amount: Order.total_amount,
+//         email_address: Order.email_address,
+//         status: Order.status,
+//         billing_address: Order.billing_address,
+//       };
+//     });
+//     res.status(200).send({ successMsg: "Here are your Orders.", Orders });
+//   } catch (error) {
+//     res.status(500).send({ errorMsg: error });
+//   }
+// };
 
 const setOrder = async (req, res, next) => {
   try {
-    let { name, total_amount, email_address, billing_address, status, UserId } =
-      req.body;
+    let { total_amount, email_address, billing_address, status, UserId } = req.body;
     console.log(req.body);
-    if (
-      !name ||
-      !total_amount ||
-      !email_address ||
-      !billing_address ||
-      !status
-    ) {
+    if (!total_amount || !email_address || !billing_address || !status) {
       res.status(402).send({ errorMsg: "Missing data." });
     } else {
       // let [newOrder, created] = await Order.findOrCreate({
@@ -55,7 +48,6 @@ const setOrder = async (req, res, next) => {
       //   : res.status(401).json({ errorMsg: "Order already exists." });
 
       let neworder = await Order.create({
-        name,
         total_amount,
         email_address,
         billing_address,
@@ -70,6 +62,6 @@ const setOrder = async (req, res, next) => {
 };
 
 module.exports = {
-  getOrders,
+  // getOrders,
   setOrder,
 };
--- client/src/components/products/ProductsStyles.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const ProductsContainer = styled.div`
-margin-top: 128px;
-`   
