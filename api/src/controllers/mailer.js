af70213cde79fedc2915837c30ce85aac2708034
Carlos Mario Espinosa Restrepo <carlosespinosa66@outlook.com>
2022-04-08 13:55:39 -0500

    UPDATE (Reset Password Mailer)

--- api/src/controllers/mailer.js
+++ api/src/controllers/mailer.js
@@ -4,7 +4,6 @@ const sendMail = async (req, res) => {
     let { emailToAddress, emailToSubject, emailToBody } = req.body;
 
     try {
-
         let info = await mailer.sendMail({
             from: 'E-commerce Henry PF" <najupasa@gmail.com>',
             to: emailToAddress,
@@ -16,30 +15,25 @@ const sendMail = async (req, res) => {
             res.status(200).send({
                 successMsg: "Mail successfully sent."
             })
-            : res.status(401).send({ errorMsg: "Mail can not be sent." });
+            : res.status(401).send({ errorMsg: "User doesn't" });
     } catch (error) {
         res.status(500).send({ errorMsg: error.message });
     }
 };
 
-const sendMailPassword = async (req, res) => {
-    let { emailToAddress, emailToSubject, emailToBody } = req.body;
+const sendMailPassword = async (emailToAddress, emailToSubject, emailToBody) => {
+    
     try {
         let info = await mailer.sendMail({
             from: 'E-commerce Henry PF" <najupasa@gmail.com>',
             to: emailToAddress,
             subject: emailToSubject,
             html: emailToBody,
-            auth: {
-                user: 'user@example.com',
-                refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-                accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
-                expires: 1484314697598.
-            }
         });
+
         info ?
             res.status(200).send({
-                successMsg: "Mail successfully sent."
+                successMsg: "Mail successfully sent.", data: token
             })
             : res.status(401).send({ errorMsg: "Mail can not be sent." });
     } catch (error) {
@@ -47,5 +41,4 @@ const sendMailPassword = async (req, res) => {
     }
 };
 
-
-module.exports = sendMail;
+module.exports ={ sendMail, sendMailPassword}
--- api/src/routes/mailer.js
+++ api/src/routes/mailer.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const sendMail = require("../controllers/mailer.js");
-const sendMailPassword = require("../controllers/mailer.js");
 
 //Creating routes and adding the controllers.
 
 const mailRouter = express.Router();
 
 mailRouter.put("/admin/email", sendMail);
-mailRouter.put("/admin/emailpwd", sendMailPassword);
 
 module.exports = mailRouter;
