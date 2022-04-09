6bd02b2bee97ae59984b3109787367695b85b422
nmacenco <nicolasmacenco@gmail.com>
2022-04-09 19:24:11 -0300

    FIX EmailResetPassword
    validations work ok thanks to Fran and Juli

--- client/src/components/users/EmailResetPassword.tsx
+++ client/src/components/users/EmailResetPassword.tsx
@@ -1,32 +1,48 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { errorMonitor } from "stream";
 import { useLocalStorage } from "../../helpers/useLocalStorage";
 import { forgotPasswordReset } from "../../redux/actions/user";
+import swal from "sweetalert";
 
 import { Container, FormContainer1 } from "./EmailResetPasswordStyles";
+import { useNavigate } from "react-router";
 
 export function validate (theEmail : any )  {
-  let error = {};
-  
-  return error ; 
+  let error = {
+    email : ''
+  };
+
+  if (!theEmail.email) {
+    error.email = "Email required.";
+  } else if (!/[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+/gm.test(theEmail.email)){
+    error.email = "It's not a valid email.";
+  }
+
+
+  return error ;  
 }
 
 export default function EmailResetPassword(): JSX.Element {
   const dispatch = useDispatch()
-
+  const navigate = useNavigate()
+  const [showError , setShowError] = useState(false) ;
   const [error, setError] = useState({
-})
+    email : 'Empty required'
+  })
   const [theEmail , setTheEmail ] = useState({
     email :  ''
   })
-  // function handleOnBlur(e) {
-  //   setError(
-  //     validate({
-  //       ...theEmail, 
-  //       [e.target.name]: e.target.value
-  //     })
-  //   );
-  // }
+  function handleOnBlur(e: any ) {
+    console.log(e.target.name);
+    
+    setError(
+      validate({
+        ...theEmail, 
+        [e.target.name]: e.target.value
+      })
+    );
+  }
   function handleOnChange (e : any ){
     e.preventDefault()
     setTheEmail({
@@ -35,8 +51,25 @@ export default function EmailResetPassword(): JSX.Element {
 
   }
   
-  function handleSubmit (){
-    dispatch(forgotPasswordReset(theEmail))
+  console.log(Object.keys(error).length);
+  function handleSubmit (e : any ){
+    setShowError(true)
+    if( error.email.length === 0 ) {
+      dispatch(forgotPasswordReset(theEmail))
+      swal({
+        title: "Email sended, check your inbox",
+        icon: "success"
+      })
+      navigate("/products")
+      
+    } else {
+      e.preventDefault()
+      swal({
+        title: "Fill field with your email.",
+        icon: "error"
+      })
+
+    }
   }
   return (
     <Container>
@@ -52,19 +85,19 @@ export default function EmailResetPassword(): JSX.Element {
                 type="email"
                 className="form-control"
                 id="staticEmail"
-                name="name"
+                name="email"
                 // value={}
                 placeholder="Type your email"
                 onChange={(e) => handleOnChange(e)}
-                // onBlur = {(e) => handleOnBlur(e) }
+                onBlur = {(e) => handleOnBlur(e) }
               />
               <p className="text-danger">
-                {/* {errorsList.name ? errorsList.name : "⠀"} */}
+                { showError  ? error.email : "⠀"}
               </p>
             </div>
 
             <div className="text-center">
-              <button onClick={()=> {handleSubmit()}} type="submit" className="btn btn-outline-primary mt-5 ">
+              <button onClick={(e)=> {handleSubmit(e)}} type="submit" className="btn btn-outline-primary mt-5 ">
                 Submit
               </button>
             </div>
