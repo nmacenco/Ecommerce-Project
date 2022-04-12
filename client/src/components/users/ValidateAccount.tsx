8dcb8b5f9dec53ab561d78382d30fc1969028897
nmacenco <nicolasmacenco@gmail.com>
2022-04-12 11:01:05 -0300

    FIX validate component, still working on it

--- client/src/components/users/ValidateAccount.tsx
+++ client/src/components/users/ValidateAccount.tsx
@@ -1,31 +1,48 @@
-
 import React from "react";
+import { useNavigate } from "react-router";
 import { Container } from "./EmailResetPasswordStyles";
-
-
+import swal from "sweetalert";
 
 export default function ValidateAccount(): JSX.Element {
-
-    function handleOnClick ( ) {
-
-    }
-    return (
-        <Container>
-
-          <div className="card-body">
-            <form >
-              <h3 className="text-center">Validate Account</h3>
-
-  
-              <div className="text-center">
-                <button onClick={(e) => { handleOnClick() }} type="submit" className="btn btn-outline-primary mt-5 ">
-                  Click here
-                </button>
-              </div>
-            </form >
-          </div >
-
-        </Container>
-    );
+  const navigate = useNavigate();
+  function handleSubmit() {
+    navigate("/login");
   }
-  
+  function handleOnClick() {
+    swal({
+      text: "Your account was validated, please login",
+      icon: "success",
+      buttons: {
+        confirm: true,
+      },
+    }).then((value) => {
+      if (value) {
+        navigate("/login");
+      }
+    });
+  }
+  return (
+    <Container>
+      <div className="card-body">
+        <form
+          onSubmit={() => {
+            handleSubmit();
+          }}
+        >
+          <h3 className="text-center">Validate Account</h3>
+          <div className="text-center">
+            <button
+              onClick={(e) => {
+                handleOnClick();
+              }}
+              type="submit"
+              className="btn btn-outline-primary mt-5 "
+            >
+              Click here
+            </button>
+          </div>
+        </form>
+      </div>
+    </Container>
+  );
+}
