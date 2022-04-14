cbae24e4afdf11f3cb5666b0dccc3d82d55c98c4
Julian Gonzalez <julianmarcos505@gmail.com>
2022-04-13 22:45:25 -0300

    orders

---
+++ client/src/redux/actions/ordersUser.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+
+import { AXIOSDATA, Product, TYPES_PRODUCT } from "../interface";
+// import interfaces from '....'
+const URL = "http://localhost:3001/api";
+
+export const getProducts = () => {
+  try {
+    return async (dispatch: Dispatch) => {
+      const allProducts = await axios.get<AXIOSDATA>(`${URL}/products`);
+      return dispatch({
+        type: TYPES_PRODUCT.GET_PRODUCTS,
+        payload: allProducts.data.data,
+      });
+    };
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const orderProducts = (value: string, products: Product[]) => {
+  try {
+    return async (dispatch: Dispatch) => {
+      return dispatch({
+        type: TYPES_PRODUCT.ORDER_PRODUCTS,
+        payload: {
+          value,
+          products,
+        },
+      });
+    };
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const selectProducts = (products: Product[]) => {
+  // console.log("SELECT PRODUCTS DISPATCH");
+  return {
+    type: TYPES_PRODUCT.SEARCH_PRODUCTS,
+    payload: products
+  };
+};
+export const resetPoducts = () => {
+  return {
+    type: TYPES_PRODUCT.RESET_PRODUCTS,
+    payload: []
+  };
+};
+export const productNotFound = (data: boolean) => {
+  return {
+    type: TYPES_PRODUCT.PRODUCT_NOT_FOUND,
+    payload: data
+  };
+};
