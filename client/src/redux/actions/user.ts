3d67335d52e97a73b5ba3d02531bdee6c539ea4e
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-04-15 10:42:17 -0500

    resolved delay wishList

--- client/src/redux/actions/user.ts
+++ client/src/redux/actions/user.ts
@@ -89,7 +89,10 @@ export const GetUSer = (email: string, pass: string, cb: any) => {
       const TOKEN = response.headers["auth-token"];
       // console.log('TOKEN: ',TOKEN);
       // console.log(response.data.data);
-      if (response.status == 200) {
+      console.log(response.data);
+      if (response.data.errorMsg) {
+        cb(); //Ejecutamos un callback wajajaj
+      } else {
         dispatch({
           type: TYPES_USER.GET_USER,
           payload: {
@@ -110,8 +113,6 @@ export const GetUSer = (email: string, pass: string, cb: any) => {
             google: false,
           })
         );
-        cb(); //Ejecutamos un callback wajajaj
-      } else {
         cb();
       }
     } catch (error) {
@@ -211,7 +212,7 @@ export const RegisterWithGoogle = (user: any, cb = defaultCb) => {
 export const LoginWithGoogle = (email: string, cb = defaultCb) => {
   return async (dispatch: Dispatch) => {
     try {
-      const response = await axios.post(URL_USER + "//signInWithGoogle", {
+      const response = await axios.post(URL_USER + "/signInWithGoogle", {
         email,
       });
 
