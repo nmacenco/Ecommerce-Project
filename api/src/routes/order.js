2563d889d37571fc142bf2479af1a16e6da5b2e0
Carlos Mario Espinosa Restrepo <carlosespinosa66@outlook.com>
2022-04-19 20:28:07 -0500

    UPDATE (Stock Update)

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -18,8 +18,8 @@ const { isLoggedIn, isAdmin } = require("../middleware/auth");
 const orderRouter = express.Router();
 
 //user
-orderRouter.post("/auth/orders", isLoggedIn, createOrder); //a new product is added to the cart here
-// orderRouter.post("/auth/orders/:UserId", createOrder); //a new product is added to the cart here
+// orderRouter.post("/auth/orders", isLoggedIn, createOrder); //a new product is added to the cart here
+orderRouter.post("/auth/orders/:UserId", createOrder); //a new product is added to the cart here
 
 orderRouter.put("/auth/orders/info/:id", isLoggedIn, updateOrder);
 orderRouter.get("/auth/orders/user", isLoggedIn, getUserOrdersServer);
@@ -31,8 +31,8 @@ orderRouter.delete(
   deleteProductsOrder
 );
 orderRouter.get("/auth/orders", isLoggedIn, getActiveOrder);
-orderRouter.put("/auth/orders/pay/:id", isLoggedIn, updatePaypalOrder);
-//  orderRouter.put("/auth/orders/pay/:id", updatePaypalOrder);
+// orderRouter.put("/auth/orders/pay/:id", isLoggedIn, updatePaypalOrder);
+ orderRouter.put("/auth/orders/pay/:id", updatePaypalOrder);
 
 //admin
 orderRouter.put(
