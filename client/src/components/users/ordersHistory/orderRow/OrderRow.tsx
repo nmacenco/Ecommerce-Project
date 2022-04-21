082be984b83f4b01720e7e97b7271d1bce549af3
Julian Gonzalez <julianmarcos505@gmail.com>
2022-04-21 01:13:55 -0300

    order history & admin styles

--- client/src/components/users/ordersHistory/orderRow/OrderRow.tsx
+++ client/src/components/users/ordersHistory/orderRow/OrderRow.tsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { getProductDetail } from "../../../../redux/actions/productDetail";
 import { Img } from "../OrdersHistoryStyle";
-import { Fifth, First, Fourth, Second, Third } from "./OrderRowStyle";
+import {  First, Fourth, Second, Third } from "./OrderRowStyle";
 
 interface Detail_Props {
   id: number;
@@ -23,36 +23,69 @@ interface Props {
   details: Detail_Props[];
 }
 
-const OrderRow = ({ id, userId,totalAmount, status, paidAt, details }: Props): JSX.Element => { 
-
+const OrderRow = ({
+  id,
+  userId,
+  totalAmount,
+  status,
+  paidAt,
+  details,
+}: Props): JSX.Element => {
   return (
     <tbody >
       <tr>
         <First scope="row">{paidAt.substr(5, 5)}</First>
         <Second>{status}</Second>
-        <Fifth></Fifth>
         <Third>${totalAmount}</Third>
-        <Fourth><button className="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${id}`} aria-expanded="false" aria-controls="collapse">Detail</button></Fourth>
-
+        <Fourth>
+          <button
+            className="btn btn-outline-primary "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={`#collapse${id}`}
+            aria-expanded="false"
+            aria-controls="collapse"
+          >
+            Detail
+          </button>
+        </Fourth>
       </tr>
-      {
-        details.map(product => {
-          return <tr key={product.productId} id={`collapse${id}`} className="accordion-collapse collapse align-items-center" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <td><Img src={product.image} /></td>
-            <td><Link to={`/detail/${product.productId}`} className="text-decoration-none">{product.productName}</Link></td>
-            {status === "FINISHED" &&
-            <td><Link to={`/newReview/${userId}/${product.productId}`} className="btn btn-primary btn-sm">
-              Review
-            </Link>
-            </td>}  
+      {details.map((product) => {
+        return (
+          <tr
+            key={product.productId}
+            id={`collapse${id}`}
+            className="accordion-collapse collapse align-items-center"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <td>
+              <Img src={product.image} />
+            </td>
+            <td>
+              <Link
+                to={`/detail/${product.productId}`}
+                className="text-decoration-none"
+              >
+                {product.productName}
+              </Link>
+              {status === "FINISHED" && (
+                <Link
+                  to={`/newReview/${userId}/${product.productId}`}
+                  className="ms-3 btn btn-primary btn-sm "
+                >
+                  Review
+                </Link>
+              )}{" "}
+            </td>
+
             <td>${product.amount}</td>
             <td>Quantity: {product.quantity}</td>
-            
           </tr>
-        })
-      }
+        );
+      })}
     </tbody>
-  )
-}
+  );
+};
 
 export default OrderRow;
