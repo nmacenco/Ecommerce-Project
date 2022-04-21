f2622bb242b565d2d0cb62316648135f4c6ef775
nmacenco <nicolasmacenco@gmail.com>
2022-04-20 21:05:20 -0300

    styles to PreviewOrder

--- client/src/components/checkout/previewOrder/PreviewOrder.tsx
+++ client/src/components/checkout/previewOrder/PreviewOrder.tsx
@@ -10,6 +10,9 @@ import {
   AdminProductIMG,
   ColumnsContainer,
   ItemsTaxShipp,
+  TableContainer,
+  OrderSumaryContainer,
+  OrderSumarySmallerContainer,
 } from "./PreviewOrderStyles";
 
 export default function PreviewOrder(): JSX.Element {
@@ -38,11 +41,12 @@ export default function PreviewOrder(): JSX.Element {
       <h3 className="mt-3 mb-5">Preview Order</h3>
       {/* <ColumnsContainer className="row justify-content-center w-100"> */}
       <ColumnsContainer>
-        <div className=" col-6">
-          <div className="card bg-secondary mb-3 ">
+        <TableContainer>
+          
+          <div className="card bg-secondary mb-3 col-xs-4 " >
+
             <div className="card-header">Shipping</div>
             <div className="card-body">
-              {/* <h4 className="card-title">Secondary card title</h4> */}
               <p className="card-text"> {activeOrder.user} </p>
               <p className="card-text"> {activeOrder.shipping_address} </p>
               <Link className="text-decoration-none" to={"/shippingAddress"}>
@@ -52,6 +56,7 @@ export default function PreviewOrder(): JSX.Element {
           </div>
           <div className="card bg-secondary mb-3 ">
             <div className="card-header">Items</div>
+            <div className="table-responsive">
             <table className="table table-hover ">
               <thead>
                 <tr>
@@ -60,7 +65,6 @@ export default function PreviewOrder(): JSX.Element {
                   <th scope=" col ">Quantity</th>
                   <th scope="col">Price</th>
                   <th scope="col">Total</th>
-                  {/* <th scope="col">POr las  </th> */}
                 </tr>
               </thead>
               {Object.keys(activeOrder).length > 0 &&
@@ -99,15 +103,110 @@ export default function PreviewOrder(): JSX.Element {
 
                         <td>$ {product.quantity * product.price} </td>
 
-                        {/* <td>{}</td> */}
                       </tr>
                     </tbody>
                   );
                 })}
             </table>
+
+            </div>
+          </div>
+        </TableContainer>
+        <OrderSumaryContainer>
+          <OrderSumarySmallerContainer>
+
+          
+            <h4 className="card-title mb-3 ">Order Summary</h4>
+            <ItemsTaxShipp>
+              <p className="card-text"> Items  </p> <p>$ {activeOrder.total_amount}</p>
+            </ItemsTaxShipp>
+            <ItemsTaxShipp>
+              <p className="card-text"> Tax  </p> <p>$ 0</p>
+            </ItemsTaxShipp>
+            <ItemsTaxShipp>
+              <p className="card-text"> Shipping  </p> <p>$ 0</p>
+            </ItemsTaxShipp>
+            <ItemsTaxShipp className="fw-bold" >
+              <p className="fw-bold"> Total   </p> <p className="fw-bold">$ {activeOrder.total_amount} </p>
+            </ItemsTaxShipp>
+            <PayPalCheckoutButtons></PayPalCheckoutButtons>
+          </OrderSumarySmallerContainer>
+        </OrderSumaryContainer>
+
+      </ColumnsContainer>
+
+      
+      {/* <ColumnsContainer>
+        <div className=" col-lg-6 sm-10  "  >
+          <div className="card bg-secondary mb-3 ">
+            <div className="card-header">Shipping</div>
+            <div className="card-body">
+              <p className="card-text"> {activeOrder.user} </p>
+              <p className="card-text"> {activeOrder.shipping_address} </p>
+              <Link className="text-decoration-none" to={"/shippingAddress"}>
+                Edit
+              </Link>
+            </div>
+          </div>
+          <div className="card bg-secondary mb-3 ">
+            <div className="card-header">Items</div>
+            <div className="table-responsive">
+            <table className="table table-hover ">
+              <thead>
+                <tr>
+                  <th scope="col">Image</th>
+                  <th scope="col">Product Name</th>
+                  <th scope=" col ">Quantity</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Total</th>
+                </tr>
+              </thead>
+              {Object.keys(activeOrder).length > 0 &&
+                activeOrder.details.map((product: any, i: any) => {
+                  return (
+                    <tbody key={i}>
+                      <tr className="table-light">
+                        <th scope="row">
+                          <AdminProductIMG
+                            src={product.image}
+                            alt={product.image}
+                            className="card-img-top"
+                          ></AdminProductIMG>
+                        </th>
+                        <td className="">
+                          {product.productName.length > 30 ? (
+                            <p>
+                              <Link
+                                className="text-decoration-none"
+                                to={`/detail/${product.productId}`}
+                              >
+                                {product.productName.slice(0, 30)}...
+                              </Link>
+                            </p>
+                          ) : (
+                            <p className="card-title m-2 text-decoration-none">
+                              <Link to={`/detail/${product.productId}`}>
+                                {product.productName}
+                              </Link>
+                            </p>
+                          )}
+                        </td>
+                        <td className=" card-title m-2">{product.quantity} </td>
+
+                        <td>$ {product.price} </td>
+
+                        <td>$ {product.quantity * product.price} </td>
+
+                      </tr>
+                    </tbody>
+                  );
+                })}
+            </table>
+
+            </div>
           </div>
         </div>
-        <div className="col-3 ms-3 h-25 border p-4">
+        <div className="col-3 ms-3 h-25 border p-4 col-sm-4 "  >
           <div className="card-body">
             <h4 className="card-title mb-3 ">Order Summary</h4>
             <ItemsTaxShipp>
@@ -125,7 +224,7 @@ export default function PreviewOrder(): JSX.Element {
             <PayPalCheckoutButtons></PayPalCheckoutButtons>
           </div>
         </div>
-      </ColumnsContainer>
+      </ColumnsContainer> */}
     </Previewcontainer>
   );
 }
