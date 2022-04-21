20781fb1ffd8b7d207ec554ffe20ae37dc67db4b
nmacenco <nicolasmacenco@gmail.com>
2022-04-21 00:03:10 -0300

    PreviewOrder responsive

--- client/src/components/checkout/previewOrder/PreviewOrder.tsx
+++ client/src/components/checkout/previewOrder/PreviewOrder.tsx
@@ -39,105 +39,8 @@ export default function PreviewOrder(): JSX.Element {
   return (
     <Previewcontainer>
       <h3 className="mt-3 mb-5">Preview Order</h3>
-      {/* <ColumnsContainer className="row justify-content-center w-100"> */}
-      {/* <ColumnsContainer>
-        <TableContainer>
-          
-          <div className="card bg-secondary mb-3 col-xs-4 " >
-
-            <div className="card-header">Shipping</div>
-            <div className="card-body">
-              <p className="card-text"> {activeOrder.user} </p>
-              <p className="card-text"> {activeOrder.shipping_address} </p>
-              <Link className="text-decoration-none" to={"/shippingAddress"}>
-                Edit
-              </Link>
-            </div>
-          </div>
-          <div className="card bg-secondary mb-3 ">
-            <div className="card-header">Items</div>
-            <div className="table-responsive">
-            <table className="table table-hover ">
-              <thead>
-                <tr>
-                  <th scope="col">Image</th>
-                  <th scope="col">Product Name</th>
-                  <th scope=" col ">Quantity</th>
-                  <th scope="col">Price</th>
-                  <th scope="col">Total</th>
-                </tr>
-              </thead>
-              {Object.keys(activeOrder).length > 0 &&
-                activeOrder.details.map((product: any, i: any) => {
-                  return (
-                    <tbody key={i}>
-                      <tr className="table-light">
-                        <th scope="row">
-                          <AdminProductIMG
-                            src={product.image}
-                            alt={product.image}
-                            className="card-img-top"
-                          ></AdminProductIMG>
-                        </th>
-                        <td className="">
-                          {product.productName.length > 30 ? (
-                            <p>
-                              <Link
-                                className="text-decoration-none"
-                                to={`/detail/${product.productId}`}
-                              >
-                                {product.productName.slice(0, 30)}...
-                              </Link>
-                            </p>
-                          ) : (
-                            <p className="card-title m-2 text-decoration-none">
-                              <Link to={`/detail/${product.productId}`}>
-                                {product.productName}
-                              </Link>
-                            </p>
-                          )}
-                        </td>
-                        <td className=" card-title m-2">{product.quantity} </td>
-
-                        <td>$ {product.price} </td>
-
-                        <td>$ {product.quantity * product.price} </td>
-
-                      </tr>
-                    </tbody>
-                  );
-                })}
-            </table>
-
-            </div>
-          </div>
-        </TableContainer>
-        <OrderSumaryContainer>
-          <OrderSumarySmallerContainer>
-
-          
-            <h4 className="card-title mb-3 ">Order Summary</h4>
-            <ItemsTaxShipp>
-              <p className="card-text"> Items  </p> <p>$ {activeOrder.total_amount}</p>
-            </ItemsTaxShipp>
-            <ItemsTaxShipp>
-              <p className="card-text"> Tax  </p> <p>$ 0</p>
-            </ItemsTaxShipp>
-            <ItemsTaxShipp>
-              <p className="card-text"> Shipping  </p> <p>$ 0</p>
-            </ItemsTaxShipp>
-            <ItemsTaxShipp className="fw-bold" >
-              <p className="fw-bold"> Total   </p> <p className="fw-bold">$ {activeOrder.total_amount} </p>
-            </ItemsTaxShipp>
-            <PayPalCheckoutButtons></PayPalCheckoutButtons>
-          </OrderSumarySmallerContainer>
-        </OrderSumaryContainer>
-
-      </ColumnsContainer> */}
-
-      
       <ColumnsContainer>
-        <div className=" col-11 col-md-6  "  >
+        <div className=" col-11 col-md-8 col-lg-8  "  >
           <div className="card bg-secondary mb-3 ">
             <div className="card-header">Shipping</div>
             <div className="card-body">
@@ -206,7 +109,7 @@ export default function PreviewOrder(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="col-11 col-md-4 ms-3 h-25 border p-4  "  >
+        <div className="col-11 col-md-5 col-lg-3 ms-3 h-25 border p-4  "  >
           <div className="card-body">
             <h4 className="card-title mb-3 ">Order Summary</h4>
             <ItemsTaxShipp>
